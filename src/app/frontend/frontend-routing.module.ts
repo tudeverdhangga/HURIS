2120b954ce62d354dfa4bd8a9550cadeb7849049
I Made Akira Ivandio Agusta <ivandioakira@gmail.com>
2021-01-29 10:37:29 +0800

    Create Employee UI

--- src/app/frontend/frontend-routing.module.ts
+++ src/app/frontend/frontend-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './employee/create/create.component';
 import { EmployeeComponent } from './employee/employee.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'employee', component: EmployeeComponent},
+  {path: 'employee/create', component: CreateComponent}
 ];
 
 @NgModule({
