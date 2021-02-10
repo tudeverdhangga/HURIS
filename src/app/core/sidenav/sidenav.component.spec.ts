012b6844c6d3cdabfa1f36fc4254f0f8891b99c2
luciahyaning <luciahyaningpergiwatiputri.kinasih@mitrais.com>
2021-02-10 18:34:14 +0800

    Add unit test examples

--- src/app/core/sidenav/sidenav.component.spec.ts
+++ src/app/core/sidenav/sidenav.component.spec.ts
@@ -25,4 +25,12 @@ describe('SidenavComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('HURIS');
   });
+
+  it(`should show side nav menu`, () => {
+    const fixture = TestBed.createComponent(SidenavComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    expect(app.sidenav.fixedTopGap).toBe(64);
+    expect(app.opened).toBeTrue();
+  });
 });
--- src/app/pages/employee/employee.component.spec.ts
+++ src/app/pages/employee/employee.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeeComponent } from './employee.component';
+
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
@@ -9,7 +10,7 @@ describe('EmployeeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ MatDialogModule ],
+      imports: [ MatDialogModule, BrowserAnimationsModule ],
       declarations: [ EmployeeComponent ]
     })
     .compileComponents();
@@ -23,5 +24,12 @@ describe('EmployeeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // test opendialog
+  it('should call onDelete function', () => {
+    spyOn(component, 'openConfirmDialog').and.callThrough();
+    component.onDelete(1);
+    expect(component.openConfirmDialog).toHaveBeenCalled();
   });
 });
